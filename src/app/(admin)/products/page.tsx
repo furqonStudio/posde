3879b0781229ee18025db533cb84f4162108151f
furqonstudio <furqonstudio@gmail.com>
2025-03-05 12:47:54 +0700

    feat: install alert-dialog ui

--- src/app/(admin)/products/page.tsx
+++ src/app/(admin)/products/page.tsx
@@ -1,7 +1,12 @@
+import { ProductsTable } from '@/components/molecules/ProductsTable'
 import React from 'react'
 
 const page = () => {
-  return <div>page</div>
+  return (
+    <div className="w-full overflow-scroll p-4">
+      <ProductsTable />
+    </div>
+  )
 }
 
 export default page
--- src/components/molecules/ProductsTable.tsx
+++ src/components/molecules/ProductsTable.tsx
@@ -13,7 +13,13 @@ import {
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react'
+import {
+  ArrowUpDown,
+  ChevronDown,
+  MoreHorizontal,
+  Pencil,
+  Trash2,
+} from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
@@ -35,139 +41,79 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import Image from 'next/image'
+import { formatCurrency } from '@/utils/format'
+import { Product } from '@/types'
+import { products } from '@/data'
 
-const data: Payment[] = [
+export const columns: ColumnDef<Product>[] = [
   {
-    id: 'm5gr84i9',
-    amount: 316,
-    status: 'success',
-    email: 'ken99@example.com',
-  },
-  {
-    id: '3u1reuv4',
-    amount: 242,
-    status: 'success',
-    email: 'Abe45@example.com',
-  },
-  {
-    id: 'derv1ws0',
-    amount: 837,
-    status: 'processing',
-    email: 'Monserrat44@example.com',
-  },
-  {
-    id: '5kma53ae',
-    amount: 874,
-    status: 'success',
-    email: 'Silas22@example.com',
-  },
-  {
-    id: 'bhqecj4p',
-    amount: 721,
-    status: 'failed',
-    email: 'carmella@example.com',
-  },
-]
-
-export type Payment = {
-  id: string
-  amount: number
-  status: 'pending' | 'processing' | 'success' | 'failed'
-  email: string
-}
-
-export const columns: ColumnDef<Payment>[] = [
-  {
-    id: 'select',
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && 'indeterminate')
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
+    accessorKey: 'image',
+    header: 'Image',
+    cell: ({ row }) => (
+      <Image
+        src={row.getValue('image')}
+        alt={row.getValue('name')}
+        width={32}
+        height={32}
+        className="rounded-sm"
       />
     ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Name
+        <ArrowUpDown />
+      </Button>
     ),
-    enableSorting: false,
-    enableHiding: false,
+    cell: ({ row }) => <div>{row.getValue('name')}</div>,
   },
   {
-    accessorKey: 'status',
-    header: 'Status',
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue('status')}</div>
-    ),
-  },
-  {
-    accessorKey: 'email',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Email
-          <ArrowUpDown />
-        </Button>
-      )
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue('email')}</div>,
-  },
-  {
-    accessorKey: 'amount',
-    header: () => <div className="text-right">Amount</div>,
+    accessorKey: 'price',
+    header: () => <div className="text-right">Price</div>,
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue('amount'))
+      const price = parseFloat(row.getValue('price'))
 
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount)
+      const formatted = formatCurrency(price)
 
       return <div className="text-right font-medium">{formatted}</div>
     },
   },
   {
+    accessorKey: 'category',
+    header: 'Category',
+    cell: ({ row }) => <div>{row.getValue('category')}</div>,
+  },
+  {
     id: 'actions',
     enableHiding: false,
     cell: ({ row }) => {
-      const payment = row.original
+      const product = row.original
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <>
+          <Button variant="ghost" size="sm" onClick={() => handleEdit(product)}>
+            <Pencil className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => handleDelete(product.id)}
+          >
+            <Trash2 className="h-4 w-4" />
+          </Button>
+        </>
       )
     },
   },
 ]
 
-export function ProductTable() {
+export function ProductsTable() {
   const [sorting, setSorting] = React.useState<SortingState>([])
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     [],
@@ -175,9 +121,14 @@ export function ProductTable() {
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({})
   const [rowSelection, setRowSelection] = React.useState({})
+  const [isEditModalOpen, setIsEditModalOpen] = React.useState(false)
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = React.useState(false)
+  const [selectedProduct, setSelectedProduct] = React.useState<Product | null>(
+    null,
+  )
 
   const table = useReactTable({
-    data,
+    data: products,
     columns,
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
@@ -195,6 +146,23 @@ export function ProductTable() {
     },
   })
 
+  const handleEdit = (product: Product) => {
+    setSelectedProduct(product)
+    setIsEditModalOpen(true)
+  }
+
+  const handleDelete = (productId: number) => {
+    setSelectedProduct(
+      products.find((product) => product.id === productId) || null,
+    )
+    setIsDeleteModalOpen(true)
+  }
+
+  const confirmDelete = () => {
+    // Add your delete logic here
+    setIsDeleteModalOpen(false)
+  }
+
   return (
     <div className="w-full">
       <div className="flex items-center justify-between pb-4">
@@ -202,10 +170,10 @@ export function ProductTable() {
 
         <div className="flex gap-4">
           <Input
-            placeholder="Filter emails..."
-            value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
+            placeholder="Filter names..."
+            value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
             onChange={(event) =>
-              table.getColumn('email')?.setFilterValue(event.target.value)
+              table.getColumn('name')?.setFilterValue(event.target.value)
             }
             className="max-w-sm"
           />
